7f90270e04ae4c7329b543bc024e60277e101ceb
Karl Keller <kgk86@hotmail.com>
2023-04-27 22:24:11 +0200

    Use flatpickr in Form

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,7 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr"
 
 // Connects to data-controller="flatpickr"
 export default class extends Controller {
   connect() {
+    flatpickr(".start_time", {
+      enableTime: true,
+      dateFormat: "F j, Y h:i K",
+    });
+    flatpickr(".end_time", {
+      enableTime: true,
+      dateFormat: "F j, Y h:i K",
+    });
   }
 }
